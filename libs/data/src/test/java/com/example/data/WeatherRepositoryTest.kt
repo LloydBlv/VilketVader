8ffb927b4c01b564e64f0fd2cc519d4d2492a0e8
Reza Najafi <mrnj92@gmail.com>
2024-03-20 13:56:39 +0100

    Add more test to WeatherRepositoryTest

--- libs/data/src/test/java/com/example/data/WeatherRepositoryTest.kt
+++ libs/data/src/test/java/com/example/data/WeatherRepositoryTest.kt
@@ -2,8 +2,10 @@ package com.example.data
 
 import assertk.Assert
 import assertk.all
+import assertk.assertFailure
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
 import assertk.assertions.prop
 import com.example.domain.Condition
 import com.example.domain.Coordination
@@ -13,15 +15,22 @@ import com.example.domain.Weather
 import com.example.domain.Wind
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import java.net.SocketTimeoutException
 
 class WeatherRepositoryTest {
     @Test
-    fun testRepositoryEmitsWeatherCorrectly() = runTest {
+    fun `repository returns mapped data correctly`() = runTest {
         val weatherRepository = com.example.testing.WeatherRepositoryFake()
         val weather = weatherRepository.getWeather("Stockholm")
-        assertThat(weather).all {
-            assertTestWeather()
-        }
+        assertThat(weather).all { assertTestWeather() }
+    }
+    @Test
+    fun `when repository throws exception is caught`() = runTest {
+        val weatherRepository = com.example.testing.WeatherRepositoryFake()
+        weatherRepository.exception = SocketTimeoutException()
+        assertFailure {
+            weatherRepository.getWeather("Stockholm")
+        }.isInstanceOf(SocketTimeoutException::class)
     }
 
 
